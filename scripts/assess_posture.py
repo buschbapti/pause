49810900f0622b23f5ccd5916ffc1a60393a8f16
buschbapti <baptiste.busch@inria.fr>
2015-09-14 12:38:15 +0200

    Add log file and deque score

--- scripts/assess_posture.py
+++ scripts/assess_posture.py
@@ -1,12 +1,20 @@
 import json
-from pause import assessment
+from pause.assessment import Assessment
+from pause import skeleton_to_joints as skel
 import sys
+
 
 if __name__ == '__main__':
     name = sys.argv[1]
-    filename = '/home/buschbapti/Documents/pause/data_test/'+name+'_test.json'
+    filename = '/tmp/'+ name + '.json'
     with open(filename) as data_file:
-        data = json.load(data_file)
-    reba = assessment.Assessment(25)
-    score = reba.assess(data)
+        skel_data = json.load(data_file)
+    # convert skeleton to joints
+    joints_data = skel.convert_skel_to_joints(skel_data)
+
+    print joints_data
+    
+    # assess the posture
+    reba = Assessment(25)
+    score = reba.assess(joints_data)
     print 'name : {}, score = {}'.format(name,score)
